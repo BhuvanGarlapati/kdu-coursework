ae7d4545567eca86386daf7ddaed48bfe0ce853d
Bhuvan G <garlabhuvan@gmail.com>
2024-01-26 18:32:16 +0530

    homework-1

---
+++ backend/homework/6/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.example.beans.Vehicle;
+import org.example.beans.VehicleService;
+import org.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.aot.ApplicationContextAotGenerator;
+
+import java.util.List;
+
+public class Main {
+    private static final LoggingSystem ls = new LoggingSystem();
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        VehicleService vehicleService = context.getBean(VehicleService.class);
+        ls.logInfo("Generated vehicles:");
+        List<Vehicle> vehicles = vehicleService.getVehicles();
+        for (Vehicle vehicle : vehicles) {
+            ls.logInfo(vehicle.toString());
+        }
+
+        ls.logInfo("\nMost expensive vehicle:");
+        ls.logInfo(vehicleService.toString());
+        ls.logInfo(vehicleService.getMostExpensiveVehicle().toString());
+
+    }
+}
---
+++ backend/homework/6/src/main/java/org/example/beans/SpeakerService.java
@@ -0,0 +1,20 @@
+package org.example.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class SpeakerService{
+    private static final double SONY_PRICE =15000;
+    private static final double BOSE_PRICE =8000;
+    @Bean
+    public Speaker generateSpeaker(){
+        if(Math.random()>0.5){
+            return new Speaker(SpeakerBrand.SONY,SONY_PRICE);
+        }
+        else{
+            return new Speaker(SpeakerBrand.BOSE,BOSE_PRICE);
+        }
+    }
+}
